05700e34a5df8661822265aeaa253b3a7fab3d67
Toon Dehaene <toon.dehaene@kuleuven.be>
2024-05-13 17:09:53 +0200

    fixed slice indexing on make_cost_unmut

--- src/directed/yen.rs
+++ src/directed/yen.rs
@@ -297,7 +297,7 @@ where
             // this gives a goal for dijkstra_internal to stop early when searching between spur and sink.
             // cost of the root path up to the spur node.
 
-            let to_spur_cost = make_cost_unmut(&root_path[0..=i], &successors);
+            let to_spur_cost = make_cost_unmut(&root_path[0..i], &successors);
             let rest_cost = match breakoff {
                 Some(b) => Some(b - to_spur_cost),
                 None => None,
